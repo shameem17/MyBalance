161d49cce5ea9890fd64adb0b659219fba1e43f0
shameem17 <shameem105455@gmail.com>
2023-12-28 13:04:18 +0600

    daily update

--- app/src/main/java/com/shameem/mybalance/LoginActivity.java
+++ app/src/main/java/com/shameem/mybalance/LoginActivity.java
@@ -8,6 +8,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,9 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -33,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseAuth auth;
     String email, password;
     ProgressBar progressBar;
+    private static final String TAG = "LoginActivity";
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +67,36 @@ public class LoginActivity extends AppCompatActivity {
                                         if (task.isSuccessful()) {
                                             // Sign in success, update UI with the signed-in user's information
                                             FirebaseUser user = auth.getCurrentUser();
-                                            progressBar.setVisibility(View.GONE);
+                                            //progressBar.setVisibility(View.GONE);
                                             Toast.makeText(LoginActivity.this,"Log in Successful: "+user.getUid(), Toast.LENGTH_SHORT).show();
+
+                                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                            DocumentReference docRef = db.collection("users").document(user.getUid());
+                                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        DocumentSnapshot document = task.getResult();
+                                                        if (document.exists()) {
+                                                            Log.i(TAG, "DocumentSnapshot data: " + document.getData());
+                                                            progressBar.setVisibility(View.GONE);
+                                                            finish();
+                                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                            intent.putExtra("email", email);
+                                                            startActivity(intent);
+                                                        } else {
+                                                            Log.i(TAG, "No such document");
+                                                        }
+                                                    } else {
+                                                        Log.i(TAG, "get failed with ", task.getException());
+                                                    }
+                                                }
+                                            });
+
+
+
+
+
                                         } else {
                                             progressBar.setVisibility(View.GONE);
                                             Toast.makeText(LoginActivity.this,"Log in Failed", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/shameem/mybalance/MainActivity.java
+++ app/src/main/java/com/shameem/mybalance/MainActivity.java
@@ -2,13 +2,24 @@ package com.shameem.mybalance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nameView, emailView, balanceView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameView = findViewById(R.id.name);
+        emailView = findViewById(R.id.email);
+        balanceView = findViewById(R.id.balance);
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+        emailView.setText(email);
     }
 }
